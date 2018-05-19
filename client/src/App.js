09d89d15a5d017876ad1abf4ba3e3c21a1c050ba
Sohail Chamadia <sohailyc@gmail.com>
2018-05-19 17:18:03 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import './App.css';
+import { Card, Button, CardTitle, CardText, CardFooter,
+  CardBody, Input, InputGroup } from 'reactstrap';
+ import axios from 'axios';
+import Table from './table.jsx'
+
+class App extends React.Component {
+
+  constructor() {
+      super();
+      this.state={
+          isSubmitted: false,
+          words: [],
+          value: 0,
+      };
+      this.frequencysubmit = this.frequencysubmit.bind(this);
+      this.handleBack = this.handleBack.bind(this);
+      this.frequencyChange = this.frequencyChange.bind(this);
+  }
+
+  handleBack(e){
+    e.preventDefault();
+    this.setState(prevState => ({
+      isSubmitted: !prevState.isSubmitted,
+      words: [],
+      value: 0,
+    }));
+  }
+
+  frequencyChange(e) {
+    e.preventDefault()
+    this.setState({value: e.target.value});
+  }
+  
+  frequencysubmit(e) {
+    e.preventDefault();
+    const {value} = this.state;
+    axios.get('/api/getwords/'+value)
+        .then(response => {
+            if(response.data){
+              this.setState(prevState => ({
+                isSubmitted: !prevState.isSubmitted,
+                words: response.data,
+              }));
+            }
+        })
+  }
+  
+  render() {
+    const {isSubmitted} = this.state;
+    if(!isSubmitted) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src="https://terriblytinytales.com/img/logo_hero.jpg" alt="logo" />
+          </header>
+          <div className="App-intro">
+            <Card style={{ backgroundColor: '#000',color: '#fff', borderColor: '#000', border: '10px' }}>
+              <CardBody>
+                <CardTitle>Frequent words</CardTitle>
+                <CardText>Enter the number of Frequent words to be displayed.</CardText>   
+                  <InputGroup>
+                    <Input placeholder="No. of words" type="number" step="1" value={this.state.value} onChange={this.frequencyChange}/>
+                  </InputGroup>
+              </CardBody>
+              <CardFooter>
+                <Button onClick={this.frequencysubmit} size="lg">SUBMIT</Button>
+              </CardFooter>
+            </Card>
+          </div>
+        </div>
+      )
+    }
+    else{
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src="https://terriblytinytales.com/img/logo_hero.jpg" alt="logo" />
+          </header>
+          <div className="App-intro">
+            <Table
+              tableHead = {['No.','Word','Count']} 
+              tableData = {this.state.words}
+            />
+            <Button onClick={this.handleBack} size="lg" block>BACK</Button>
+          </div>
+        </div>
+      )
+    }
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var app = express();
+const PORT = process.env.PORT || 5000;
+var axios = require('axios');
+
+function countWords(sentence) {
+	var frequency = {};
+	var words = sentence
+              .replace(/[.,?!;()"'-]/g, " ")
+              .replace(/\s+/g, " ")
+              .toLowerCase()
+              .split(" ");
+    var uniq = []
+    words.forEach(function (word) {
+        if (!(frequency.hasOwnProperty(word))) {
+            frequency[word] = 0;
+            uniq.push(word)
+        }
+        frequency[word]++;
+    });
+    uniq.sort(function(a,b){return(frequency[b]-frequency[a])})
+    return [uniq,frequency];
+}
+
+app.get('/api/getwords/:value', function(request, response) {
+    var value = request.params.value;
+	axios.get('http://terriblytinytales.com/test.txt')
+		.then(function (res) {
+			var data = countWords(res.data)
+			var uniq = data[0]
+			var frequency = data[1]
+			var resp = []
+			if (value>uniq.length){
+				data.forEach(function(word){
+					resp.push([word,frequency[word]])
+				})
+			}
+			else{
+				for (var i = 0; i < value; i++) {
+					resp.push([uniq[i],frequency[uniq[i]]])
+				}
+			}
+			response.send(resp)
+		})
+	 	.catch(function (error) {
+	    	response.send(error);
+	 	});
+});
+
+
+app.listen(PORT, function () {
+    console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
+});
